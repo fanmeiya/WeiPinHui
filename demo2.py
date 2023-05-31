dd9b7433071d2788dcb6f73ffaad5e462cc75256
fanmeiya <fmy18081585159@outlook.com>
2023-05-31 14:49:06 +0800

    FMY提交0.00073得分的源代码

---
+++ demo2.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# @FileName  :demo2.py
+# @Time      :2023/5/23 10:26
+# @Author    :FMY
+ 
+ 
+if __name__ == "__main__":
+    run_code = 0
---
+++ demo3.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# @FileName  :demo3.py
+# @Time      :2023/5/23 11:17
+# @Author    :FMY
+ 
+ 
+if __name__ == "__main__":
+    run_code = 0
